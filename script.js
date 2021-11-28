c75fb362f8ea1103add2463cec2a0c5401637dcf
Shane Wiens <Shanewiens@gmail.com>
2021-11-28 12:29:20 -0800

    Remote Merge

--- script.js
+++ script.js
@@ -1,23 +1,50 @@
 var buttonEl = document.querySelector("#start-btn");
 
-
 var timeEl = document.querySelector("#playTimer");
+
 var questionEl = document.querySelector("#question-content");
+
 var scoreEl = document.querySelector("#hi-scores");
+
 var startEl = document.querySelector("#start-screen");
+
 var currentQuestionEl = document.querySelector("#question");
+
 var currentAnswersEl = document.querySelector("#answers");
-var currentScore = document.querySelector("#playerScore")
+
+var currentScoreEl = document.querySelector("#playerScore")
+
 var questionActive = [
     {
-        Question: "test",
-        Answers: ['a', 'b','c'],
-        correctAnswer: "b",
+        question: "Which of these is not a valid data type?",
+        answersBtns: ["booleans", "alerts", "numbers", "strings"],
+        answer: "alerts"
+    },
+    {
+        question: "Document.queryselector('#data') would wfind hat element(s)?",
+        answersBtns: ["First element with id = 'data'", "all elements with class = 'data'", "Last element with class 'data'", "all elements with class= 'data'"],
+        answer: "First element with id = 'data'",
+    },
+    {
+        question: "To combine arrays we use which command?",
+        answersBtns: [".addtolist", ".concat", ".combine", ".uniteArray"],
+        answer: ".concat",
+    },
+    {
+        question: "Which of these dictates strict equality?",
+        answersBtns: ["===", "!==", "==", ">=="],
+        answer: "==="
+    },
+    {
+        question: "Which of these is not a way to interact with the user using Javascript?",
+        answersBtns: ["prompts", "alerts", "confirms", "console log"],
+        answer: "console log"
     },
 ];
+
 var timeLeft = 25;
-currentScore = 0;
-currentScore.textContent = currentScore;
+var currentScore = 0;
+currentScoreEl.textContent = "Current score: " + currentScore;
 function displayTime() {
     startEl.setAttribute("class", "hide");
     questionEl.classList.remove("hide");
@@ -35,9 +62,9 @@ function displayTime() {
 };
 
 function askQuestions() {
-    currentQuestionEl.textContent = questionActive[0].Question;
+    currentQuestionEl.textContent = questionActive[i].question;
 
-    questionActive[0].Answers.forEach(function(answer, i){
+    questionActive[i].answer.forEach(function(answer, i){
     
         var answersBtns= document.createElement('button');
         answersBtns.setAttribute('class', 'answerBtn');
@@ -47,7 +74,7 @@ function askQuestions() {
         answersBtns.onclick = answerClick;
 
         currentAnswersEl.append(answersBtns);
-    })
+    });
 
     //for (i = 0, i < questionActive[0].Answers.length; i++;){
         
