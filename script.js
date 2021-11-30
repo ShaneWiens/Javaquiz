47826af24c740a6df2cce98e5ed387cd997e57aa
Shane Wiens <Shanewiens@gmail.com>
2021-11-29 17:18:11 -0800

    More work with Vinnie has helped me surpass the previous obstactle, still the final scoreboard taunts me. But I persist.
    For real, big shout out to Vinnie got helping me seperate my code into
    understandable bits

--- script.js
+++ script.js
@@ -14,6 +14,9 @@ var currentAnswersEl = document.querySelector("#answers");
 
 var currentScoreEl = document.querySelector("#playerScore")
 
+var finalScoreEl = document.querySelector("#finalUserScores")
+var finalUserScoreEl = document.querySelector("#finalScore")
+var submitScore = document.querySelector("#submit")
 var questionActive = [
     {
         question: "Which of these is not a valid data type?",
@@ -43,12 +46,16 @@ var questionActive = [
 ];
 
 var timeLeft = 25;
+
 var currentScore = 0;
+var questionIndex = 0;
+var timerInterval;
 currentScoreEl.textContent = "Current score: " + currentScore;
+
 function displayTime() {
     startEl.setAttribute("class", "hide");
     questionEl.classList.remove("hide");
-    var timerInterval = setInterval(function () {
+    timerInterval = setInterval(function () {
         timeLeft--;
         timeEl.textContent = timeLeft + "s remaining";
 
@@ -62,8 +69,10 @@ function displayTime() {
 };
 
 function askFirstQuestion() {
-    var questionIndex = 0;
+
     currentQuestionEl.textContent = questionActive[questionIndex].question;
+    currentAnswersEl.innerHTML = '';
+
     questionActive[questionIndex].answers.forEach(function (answers) {
 
         var answersBtns = document.createElement('button');
@@ -76,83 +85,91 @@ function askFirstQuestion() {
         currentAnswersEl.append(answersBtns);
 
     })
+}
 
-    //for (i = 0, i < questionActive[0].Answers.length; i++;){
+function answerClick() {
+    console.log(this.value)
 
-    // var answersBtns= document.createElement('button');
-    // answersBtns.setAttribute('class', 'answerBtn');
-    // answersBtns.setAttribute('value', questionActive[0].Answers[i]);
-
-    // answersBtns.textContent = questionActive[0].Answers[i];
-
-    // currentAnswersEl.append(answersBtns);
-    //}
-    function answerClick(event) {
-
-        console.log(this.value)
-        var userAwnser = event.target.value;
-
-        if (userAwnser === questionActive[questionIndex].correctAnswer) {
-            currentScore = currentScore + 2;
-            currentScoreEl.textContent = "Current score: " + currentScore;
-        }
-        else {
-            timeLeft = timeLeft - 2;
-            timeEl.textContent = timeLeft + "s remaining";
-        }
-        console.log(userAwnser)
-        console.log(questionActive[questionIndex].correctAnswer)
-        console.log(currentScore)
-        if (questionIndex > 4) {
-            currentScore = currentScore + timeLeft;
-            clearInterval(timerInterval);
-            endGame();
-        }
-        else {
-            askNextQuestion();
-            function askNextQuestion() {
-
-                function removeAnswers(currentAnswersEl) {
-                    while (currentAnswersEl.firstChild) {
-                        currentAnswersEl.removeChild(currentAnswersEl.firstChild);
-                    }
-                }
-                const lastAnswers = document.querySelector("#answers");
-                removeAnswers(lastAnswers)
-                questionIndex++;
-                // currentQuestionEl.textContent = questionActive[questionIndex].question;
-                // for (i = 0, i < questionActive[questionIndex].answers.length; i++;) {
-                //     var placeHolder = document.getElementById("answers").child[i];
-                //     placeHolder.setAttribute('class', 'answerBtn');
-                //     placeHolder.setAttribute('value', questionActive[questionIndex].answers[i]);
-                //     placeHolder.textContent = questionActive[questionIndex].answers[i];
-                // }
-                currentQuestionEl.textContent = questionActive[questionIndex].question;
-                questionActive[questionIndex].answers.forEach(function (answers) {
-
-                    var nextAnswersBtns = document.createElement('button');
-                    nextAnswersBtns.setAttribute('class', 'answerBtn');
-                    nextAnswersBtns.setAttribute('value', answers);
-                    nextAnswersBtns.textContent = answers;
-
-                    nextAnswersBtns.onclick = answerClick;
-
-                    currentAnswersEl.append(nextAnswersBtns);
-                })
-            }
-        }
-
+    if (this.value === questionActive[questionIndex].correctAnswer) {
+        currentScore = currentScore + 2;
+        currentScoreEl.textContent = "Current score: " + currentScore;
     }
+    else {
+        timeLeft -= 2;
+        timeEl.textContent = timeLeft + "s remaining";
+    }
+
+    questionIndex++;
+
+
+    // console.log(questionActive[questionIndex].correctAnswer)
+    // console.log(currentScore)
+    if (questionIndex === questionActive.length) {
+        currentScore = currentScore + timeLeft;
+
+        endGame();
+    }
+    else {
+        askFirstQuestion()
+    }
+
 }
 
 
-
-
+var finalScore = 0
 
 function endGame() {
-    alert("Game Over")
-    
+    finalScore = currentScore + timeLeft;
+    clearInterval(timerInterval)
+    console.log(finalScore)
+    finalUserScoreEl.innerHTML = finalScore;
+    finalScoreEl.classList.remove("hide")
+    questionEl.classList.add("hide")
+
+}
+
+function recordScore() {
+    var initialsEl = document.querySelector("#initials").value;
+    var highScores = JSON.parse(localStorage.getItem('localScores')) || [];
+
+    var newScore = {
+        initials: initialsEl,
+        score: finalScore
+    }
+
+    highScores.push(newScore);
+    localStorage.setItem('localScores', JSON.stringify(highScores))
+    topScores()
+}
+var olEl = document.querySelector("#scoreList")
+var scoreListEl = document.querySelector("#hi-scores")
+// grab and sort local storage, create elements to house scores
+function topScores() {
+    finalScoreEl.classList.add("hide")
+    scoreListEl.classList.remove("hide")
+    var highScores = JSON.parse(localStorage.getItem('localScores')) || [];
+    console.log(highScores)
+    // highScores.sort(function (a, b) {
+    //     return b.score - a.score;
+    // });
+    highScores.forEach(function (score) {
+console.log(score.initials)
+        //create our element
+        var liEl = document.createElement('li');
+
+        //add the text content to the element 
+        liEl.setAttribute('value',score);
+        liEl.textContent = score.initials + ' - ' + score.score;
+
+        //append the element to the ol
+        
+         olEl.append(liEl);
+    })
 }
 
 
+submitScore.addEventListener("click", recordScore);
 buttonEl.addEventListener("click", displayTime);
+document.querySelector('#restart').addEventListener('click', function(){
+    window.location.reload()
+})
